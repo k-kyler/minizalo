1ffc18be9b0ee422a1b145e74e4517a497db4e6d
k-kyler <khaiquang690@gmail.com>
2021-11-03 22:29:16 +0700

    Refactor sign in & sign up code

--- client/minizalo/src/pages/SignUp/index.tsx
+++ client/minizalo/src/pages/SignUp/index.tsx
@@ -9,7 +9,7 @@ export const SignUp: FC = () => {
       <div className="signup__container">
         <h1 className="signup__heading">SIGN UP</h1>
         <div className="signup__field">
-          <form action="/signup" className="signup__form">
+          <form className="signup__form">
             <input
               type="text"
               className="signup__input name__input"
@@ -41,7 +41,6 @@ export const SignUp: FC = () => {
             </label>
             <Box sx={{ height: 25 }} />
             <Button
-              type="submit"
               variant="contained"
               className="signup__submit"
               style={{ backgroundColor: "#1B86F9", color: "#FFFFFF" }}
