29a91bfe2e7f09aa479c92445021a41795a714b9
k-kyler <khaiquang690@gmail.com>
2021-11-18 18:12:14 +0700

    Configure SignalR for real-time messaging

--- server/Controllers/MessageController.cs
+++ server/Controllers/MessageController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using minizalo.Dtos;
 using minizalo.Entities;
 using minizalo.Helpers;
+using minizalo.Hubs;
 using minizalo.Repositories;
 
 namespace minizalo.Controllers
@@ -14,11 +16,13 @@ namespace minizalo.Controllers
     {
         private readonly IMessageRepository _messageRepository;
         private readonly JwtService _jwtService;
+        private readonly IHubContext<ChatHub, IChatClient> _chatHub;
 
-        public MessageController(IMessageRepository messageRepository, JwtService jwtService)
+        public MessageController(IMessageRepository messageRepository, JwtService jwtService, IHubContext<ChatHub, IChatClient> chatHub)
         {
             _messageRepository = messageRepository;
             _jwtService = jwtService;
+            _chatHub = chatHub;
         }
 
         [HttpPost("create")]
@@ -41,6 +45,7 @@ namespace minizalo.Controllers
                 };
     
                 await _messageRepository.CreateMessage(message);
+                await _chatHub.Clients.All.ReceiveMessage(message);
                 
                 return Ok(new { code = "success", message });       
             } catch (Exception ex) {
