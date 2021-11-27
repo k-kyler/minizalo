caaab8508c99d2457081ee8b25649dbb810a0216
k-kyler <khaiquang690@gmail.com>
2021-11-27 23:30:29 +0700

    Add search page & friends page

--- client/src/components/FriendsList/index.tsx
+++ client/src/components/FriendsList/index.tsx
@@ -1,23 +1,42 @@
 import { Grid, Typography } from "@mui/material";
 import { FC } from "react";
-import { selectedFriends } from "../../redux/FriendsSlice";
+import { selectFriends } from "../../redux/FriendsSlice";
+import { selectUser } from "../../redux/UserSlice";
 import { useAppSelector } from "../../redux/hooks";
 import { FriendItem } from "./FriendItem";
 import "./FriendsList.css";
 
 export const FriendsList: FC = () => {
-  const { friends } = useAppSelector(selectedFriends);
+  const { friends } = useAppSelector(selectFriends);
+  const { user } = useAppSelector(selectUser);
+
+  const friendsToDisplay = friends.filter(
+    (friend) => friend.friendId !== user.userId
+  );
 
   return (
     <div className="friendsList">
-      <Typography variant="h5" sx={{ mb: 2 }}>
-        Friends
+      <Typography variant="h5" sx={{ mb: 2, textAlign: "center" }}>
+        Friends List
       </Typography>
 
       <Grid className="friendsList__container" container>
-        {friends.map((friend) => (
-          <FriendItem key={friend.friendId} {...friend} />
-        ))}
+        {friendsToDisplay.length ? (
+          friendsToDisplay.map((friend) => (
+            <FriendItem key={friend.friendId} {...friend} />
+          ))
+        ) : (
+          <Grid item md={12}>
+            <Typography
+              variant="body1"
+              color="GrayText"
+              sx={{ textAlign: "center" }}
+              gutterBottom
+            >
+              You've no friends
+            </Typography>
+          </Grid>
+        )}
       </Grid>
     </div>
   );
