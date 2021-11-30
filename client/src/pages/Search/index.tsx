5c8073ba8f59891e617b6d016297fe6e392b0dbe
k-kyler <khaiquang690@gmail.com>
2021-11-30 09:17:17 +0700

    Fix search friend

--- client/src/pages/Search/index.tsx
+++ client/src/pages/Search/index.tsx
@@ -5,15 +5,24 @@ import { IconButton, Typography } from "@mui/material";
 import "./Search.css";
 import { SearchResults } from "../../components/SearchResults";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { searchForFriends, selectFriends } from "../../redux/FriendsSlice";
+import {
+  fetchFriendsList,
+  searchForFriends,
+  selectFriends,
+} from "../../redux/FriendsSlice";
 import { PageLoading } from "../../components/Loadings/PageLoading";
 import { useRedirect } from "../../hooks/useRedirect";
 
 export const Search: FC = () => {
   const dispatch = useAppDispatch();
 
-  const { isSearching, searchResults, searchKeyword } =
-    useAppSelector(selectFriends);
+  const {
+    isSearching,
+    searchResults,
+    searchKeyword,
+    isFetching,
+    sendingFriendRequest,
+  } = useAppSelector(selectFriends);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -27,97 +36,110 @@ export const Search: FC = () => {
     }
   };
 
-  useEffect(setPathnameHandler, []);
+  useEffect(() => {
+    dispatch(fetchFriendsList());
+    setPathnameHandler();
+  }, [sendingFriendRequest]);
 
   return (
-    <div className="search">
-      {/* Search bar */}
-      <div className="search__bar">
-        <input
-          ref={inputRef}
-          type="text"
-          className="search__input"
-          autoFocus
-          placeholder="Search for people..."
-          onKeyUp={(event) =>
-            event.key === "Enter" ? searchHandler() : () => {}
-          }
-        />
-        <IconButton className="search__button" onClick={searchHandler}>
-          <SearchIcon />
-        </IconButton>
-      </div>
-
-      {isSearching ? (
+    <>
+      {isFetching ? (
         <PageLoading />
       ) : (
-        <>
-          {/* Results title */}
-          {searchResults.length ? (
-            <Typography variant="h5" color="GrayText" sx={{ mt: 4, mb: 2.5 }}>
-              Search results for <q>{searchKeyword}</q>
-            </Typography>
-          ) : null}
+        <div className="search">
+          {/* Search bar */}
+          <div className="search__bar">
+            <input
+              ref={inputRef}
+              type="text"
+              className="search__input"
+              autoFocus
+              placeholder="Search for people..."
+              onKeyUp={(event) =>
+                event.key === "Enter" ? searchHandler() : () => {}
+              }
+            />
+            <IconButton className="search__button" onClick={searchHandler}>
+              <SearchIcon />
+            </IconButton>
+          </div>
 
-          {/* Recent searches */}
-          {/* Do it later... */}
-
-          {/* Suggest keywords */}
-          {!searchResults.length && !searchKeyword ? (
-            <div className="search__suggestKeywords">
-              <Typography
-                variant="h5"
-                color="GrayText"
-                sx={{
-                  mt: 2,
-                  mb: 2.5,
-                  textAlign: "center",
-                  fontSize: "1.75rem",
-                }}
-              >
-                Try searching for
-              </Typography>
-
-              <div className="search__suggest">
-                <NumbersIcon />
-                <Typography variant="body2" sx={{ ml: 1.25 }}>
-                  People you may know
+          {isSearching ? (
+            <PageLoading />
+          ) : (
+            <>
+              {/* Results title */}
+              {searchResults.length ? (
+                <Typography
+                  variant="h5"
+                  color="GrayText"
+                  sx={{ mt: 4, mb: 2.5 }}
+                >
+                  Search results for <q>{searchKeyword}</q>
                 </Typography>
-              </div>
+              ) : null}
 
-              <div className="search__suggest">
-                <NumbersIcon />
-                <Typography variant="body2" sx={{ ml: 1.25 }}>
-                  People you may interested in
+              {/* Recent searches */}
+              {/* Do it later... */}
+
+              {/* Suggest keywords */}
+              {!searchResults.length && !searchKeyword ? (
+                <div className="search__suggestKeywords">
+                  <Typography
+                    variant="h5"
+                    color="GrayText"
+                    sx={{
+                      mt: 2,
+                      mb: 2.5,
+                      textAlign: "center",
+                      fontSize: "1.75rem",
+                    }}
+                  >
+                    Try searching for
+                  </Typography>
+
+                  <div className="search__suggest">
+                    <NumbersIcon />
+                    <Typography variant="body2" sx={{ ml: 1.25 }}>
+                      People you may know
+                    </Typography>
+                  </div>
+
+                  <div className="search__suggest">
+                    <NumbersIcon />
+                    <Typography variant="body2" sx={{ ml: 1.25 }}>
+                      People you may interested in
+                    </Typography>
+                  </div>
+
+                  <div className="search__suggest">
+                    <NumbersIcon />
+                    <Typography variant="body2" sx={{ ml: 1.25 }}>
+                      Friends of friends
+                    </Typography>
+                  </div>
+                </div>
+              ) : !searchResults.length && searchKeyword ? (
+                <Typography
+                  variant="h5"
+                  color="GrayText"
+                  sx={{
+                    mt: 2,
+                    mb: 2.5,
+                    textAlign: "center",
+                    fontSize: "1.75rem",
+                  }}
+                >
+                  No results found
                 </Typography>
-              </div>
+              ) : null}
 
-              <div className="search__suggest">
-                <NumbersIcon />
-                <Typography variant="body2" sx={{ ml: 1.25 }}>
-                  Friends of friends
-                </Typography>
-              </div>
-            </div>
-          ) : !searchResults.length && searchKeyword ? (
-            <Typography
-              variant="h5"
-              color="GrayText"
-              sx={{
-                mt: 2,
-                mb: 2.5,
-                textAlign: "center",
-                fontSize: "1.75rem",
-              }}
-            >
-              No results found
-            </Typography>
-          ) : null}
-
-          {/* Results */}
-          {searchResults.length ? <SearchResults /> : null}
-        </>
+              {/* Results */}
+              {searchResults.length ? <SearchResults /> : null}
+            </>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 };
