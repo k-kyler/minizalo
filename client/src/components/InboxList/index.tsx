53672200ab94d083b4fbc5dc7825146129afaa2d
k-kyler <khaiquang690@gmail.com>
2022-02-05 15:37:37 +0700

    Add auto sort inbox list by latest messages, fix some logical UI

--- client/src/components/InboxList/index.tsx
+++ client/src/components/InboxList/index.tsx
@@ -1,5 +1,5 @@
 import { IconButton, Tooltip, Typography } from "@mui/material";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import "./InboxList.css";
 import CreateIcon from "@mui/icons-material/Create";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
@@ -7,13 +7,35 @@ import { InboxItem } from "./InboxItem";
 import { CustomDialog } from "../CustomDialog";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { changeSelectedInboxId, selectInboxes } from "../../redux/InboxesSlice";
-import { openDialog, selectDialog } from "../../redux/DialogSlice";
+import { openDialog } from "../../redux/DialogSlice";
+import { InboxItemType } from "../../typings/InboxItemType";
 
 export const InboxList: FC = () => {
   const dispatch = useAppDispatch();
 
   const { selectedInboxId, inboxes } = useAppSelector(selectInboxes);
-  const dialog = useAppSelector(selectDialog);
+
+  const [inboxList, setInboxList] = useState<InboxItemType[]>([]);
+
+  // Sort inbox list by its latest message
+  const sortInboxesWhenNewMessageCome = () => {
+    const sortedInboxes = [...inboxes].sort((a, b) => {
+      let aMessages = a.messages as any;
+      let bMessages = b.messages as any;
+
+      if (aMessages.length && bMessages.length)
+        return bMessages[bMessages.length - 1].createdAt.localeCompare(
+          aMessages[aMessages.length - 1].createdAt
+        );
+    });
+
+    setInboxList(sortedInboxes);
+  };
+
+  useEffect(() => {
+    sortInboxesWhenNewMessageCome();
+  }, [inboxes]);
+  // End of sort inbox list by its latest message
 
   return (
     <div className="inboxList">
@@ -45,7 +67,7 @@ export const InboxList: FC = () => {
 
       {/* Inboxes */}
       <div className="inboxList__inboxes">
-        {inboxes.map((room) => (
+        {inboxList.map((room) => (
           <InboxItem
             key={room.inboxId}
             {...room}
